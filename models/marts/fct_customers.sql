b70f8eb9bf32a69e649a64e7d91e04ab83059381
Karan Arora <karan@onprem.com>
2020-02-20 22:29:26 +0000

    update structure

--- models/marts/fct_customers.sql
+++ models/marts/fct_customers.sql
@@ -8,7 +8,7 @@ with customers as (
     select * from {{ ref('stg_customers') }}
 ),
 orders as (
-    select * from {{ ref('orders') }}
+    select * from {{ ref('dim_orders') }}
 ),
 customer_orders as (
     select
